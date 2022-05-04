ec12c638ad8a832af4a04b958acab894f18d9a10
elcampones <p.campones@campus.fct.unl.pt>
2022-05-04 10:17:21 +0100

    Remove unused functionalities from main

--- src/main/Main.java
+++ src/main/Main.java
@@ -7,27 +7,17 @@ import broadcastProtocols.lazyPush.LazyPushBroadcast;
 import broadcastProtocols.lazyPush.messages.LazyValMessage;
 import catecoin.blockConstructors.*;
 import catecoin.blocks.SimpleBlockContentList;
-import catecoin.clients.AutomatedClient;
-import catecoin.clients.InteractiveClient;
 import catecoin.mempoolManager.*;
-import catecoin.posSpecific.accountManagers.AccountManager;
-import catecoin.posSpecific.accountManagers.StatefulAccountManager;
-import catecoin.posSpecific.keyBlockManagers.ConstantWeightLedgerKeyBlockManager;
-import catecoin.posSpecific.keyBlockManagers.KeyBlockManager;
 import catecoin.transactionGenerators.FakeTxsGenerator;
-import catecoin.transactionGenerators.TransactionGenerator;
 import catecoin.txs.SlimTransaction;
 import catecoin.txs.StructuredValueSlimTransactionWrapper;
 import catecoin.validators.*;
-import chatApp.ChatApplication;
 import com.google.gson.Gson;
 import intermediateConsensus.SingleElementCommittee;
 import ledger.BabelLedger;
-import ledger.Ledger;
 import ledger.blockchain.Blockchain;
 import ledger.blocks.BlockContent;
 import ledger.blocks.BlockmessBlockImp;
-import ledger.blocks.LedgerBlock;
 import ledger.ledgerManager.LedgerManager;
 import ledger.ledgerManager.StructuredValue;
 import ledger.prototype.LedgerPrototype;
@@ -45,19 +35,11 @@ import pt.unl.fct.di.novasys.babel.core.GenericProtocol;
 import pt.unl.fct.di.novasys.babel.exceptions.HandlerRegistrationException;
 import pt.unl.fct.di.novasys.babel.exceptions.ProtocolAlreadyExistsException;
 import pt.unl.fct.di.novasys.network.data.Host;
-import sybilResistantCommitteeElection.SybilElectionProof;
-import sybilResistantCommitteeElection.poet.drand.PoETDRandProof;
-import sybilResistantCommitteeElection.poet.drand.PoETWithDRand;
 import sybilResistantCommitteeElection.poet.gpoet.BlockmessGPoET;
 import sybilResistantCommitteeElection.poet.gpoet.BlockmessGPoETProof;
-import sybilResistantCommitteeElection.poet.gpoet.GPoET;
-import sybilResistantCommitteeElection.poet.gpoet.GPoETProof;
-import sybilResistantCommitteeElection.pos.sortition.PoSAlgorandSortitionWithDRand;
-import sybilResistantCommitteeElection.pos.sortition.proofs.SortitionProof;
 import utils.InterfaceToIp;
 import valueDispatcher.ValueDispatcher;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
@@ -71,7 +53,6 @@ import java.util.stream.Collectors;
 
 import static broadcastProtocols.PeriodicPrunableHashMap.MESSAGE_PRUNE_PERIOD;
 import static broadcastProtocols.PeriodicPrunableHashMap.PERIOD_BUFFER_CAPACITY;
-import static catecoin.mempoolManager.MempoolManagerFactory.getMinimalistMempoolManager;
 import static java.lang.Integer.parseInt;
 
 public class Main {
@@ -80,7 +61,6 @@ public class Main {
 
     //Sets the log4j (logging library) configuration file
     static {
-        //System.setProperty("logFileName", "log_6000");
         System.setProperty("log4j.configurationFile", "config/log4j2.xml");
     }
 
@@ -102,21 +82,11 @@ public class Main {
         int port = getNodePort(props);
         Host myself = new Host(InetAddress.getByName(props.getProperty("address")), port);
         logger.info("Hello, I am {} and my contact is {}.", myself, props.getProperty("contact"));
-        switch (props.getProperty("executionType", "distributedLedger")) {
-            case "chat":
-                launchChatApplication(props, myself, babel);
-                break;
-            case "distributedLedger":
-                launchTraditionalDistributedLedger(props, myself, babel);
-                break;
-            case "blockmess":
-                launchBlockmess(props, myself, babel);
-                break;
-        }
+        launchBlockmess(props, myself, babel);
         Runtime.getRuntime().addShutdownHook(new Thread(() -> logger.info("Goodbye")));
     }
 
-    private static void redirectOutput(Properties props) throws FileNotFoundException {
+    private static void redirectOutput(Properties props) {
         String redirectionFile = props.getProperty("redirectFile");
         System.setProperty("logFileName", redirectionFile);
         org.apache.logging.log4j.core.LoggerContext ctx =
@@ -149,110 +119,6 @@ public class Main {
         } catch (IOException e) {
             return false;
         }
-    }
-
-    private static void launchChatApplication(Properties props, Host myself, Babel babel) throws Exception {
-        List<GenericProtocol> protocols = new LinkedList<>(addNetworkProtocols(props, myself));
-        protocols.add(new ValueDispatcher<>());
-        protocols.add(new ChatApplication(props));
-        initializeProtocols(props, babel, protocols);
-    }
-
-    private static void launchTraditionalDistributedLedger(Properties props, Host myself, Babel babel)
-            throws Exception {
-        switch (props.getProperty("sybilElectionType", "gpoet")) {
-            case "poetDrand":
-                launchPoETWithDRandDL(props, myself, babel);
-                break;
-            case "sortition":
-                launchPoSSortitionWithDRandDL(props, myself, babel);
-                break;
-            case "gpoet":
-                launchGPoET(props, myself, babel);
-                break;
-            default:
-                logger.error("Unable to identify launch type. Exiting");
-                System.exit(1);
-        }
-    }
-
-    private static void launchPoETWithDRandDL(Properties props, Host myself, Babel babel) throws Exception {
-        KeyPair myKeys = CryptographicUtils.getNodeKeys(props);
-        List<GenericProtocol> protocols = new LinkedList<>(addNetworkProtocols(props, myself));
-        MempoolManager<SlimTransaction,PoETDRandProof> mempoolManager = getMinimalistMempoolManager(props);
-        protocols.add(mempoolManager);
-        var blockDirector = BlockDirectorKits.getSimpleLedgerBlock(props, mempoolManager, myKeys);
-        GenericProtocol babelBlockDirector = new BabelBlockDirector<>(blockDirector);
-        protocols.add(babelBlockDirector);
-        TxsLoader<SlimTransaction> txsLoader = new TxLoaderImp(blockDirector);
-        Optional<TransactionGenerator> transactionGenerator = loadTxsIfNecessary(props, myKeys, txsLoader);
-        transactionGenerator.ifPresent(protocols::add);
-        var appValidator = BlockValidatorFactory.getPoETValidator(props, mempoolManager);
-        protocols.add((GenericProtocol) appValidator);
-        var ledger = new Blockchain<>(props, appValidator, new MinimalistBootstrapModule());
-        var babelLedger = new BabelLedger<>(ledger);
-        protocols.add(babelLedger);
-        protocols.addAll(addCommonDLProtocols(props, myKeys, ledger));
-        protocols.add(new PoETWithDRand<>(props, myKeys, ledger, blockDirector));
-        initializeProtocols(props, babel, protocols);
-    }
-
-    private static void launchGPoET(Properties props, Host myself, Babel babel) throws Exception {
-        KeyPair myKeys = CryptographicUtils.getNodeKeys(props);
-        List<GenericProtocol> protocols = new LinkedList<>(addNetworkProtocols(props, myself));
-        MempoolManager<SlimTransaction, GPoETProof> mempoolManager = getMinimalistMempoolManager(props);
-        protocols.add(mempoolManager);
-        var blockDirector = BlockDirectorKits.getSimpleLedgerBlock(props, mempoolManager, myKeys);
-        GenericProtocol babelBlockDirector = new BabelBlockDirector<>(blockDirector);
-        protocols.add(babelBlockDirector);
-        TxsLoader<SlimTransaction> txsLoader = new TxLoaderImp(blockDirector);
-        Optional<TransactionGenerator> transactionGenerator = loadTxsIfNecessary(props, myKeys, txsLoader);
-        transactionGenerator.ifPresent(protocols::add);
-        var appValidator = new GPoETValidator(props);
-        var ledger = new Blockchain<>(props, appValidator, new MinimalistBootstrapModule());
-        var babelLedger = new BabelLedger<>(ledger);
-        protocols.add(babelLedger);
-        protocols.addAll(addCommonDLProtocols(props, myKeys, ledger));
-        protocols.add(new GPoET<>(props, ledger, blockDirector, myKeys));
-        ledger.attachObserver(new UnfinalizedBlocksLog(props));
-        ledger.attachObserver(new FinalizedBlocksLog(props));
-        initializeProtocols(props, babel, protocols);
-    }
-
-    private static void launchPoSSortitionWithDRandDL(Properties props, Host myself, Babel babel)
-            throws Exception {
-        KeyPair myKeys = CryptographicUtils.getNodeKeys(props);
-        List<GenericProtocol> protocols = new LinkedList<>(addNetworkProtocols(props, myself));
-        MempoolManager<SlimTransaction, SortitionProof> mempoolManager =
-                MempoolManagerFactory.getRichMempoolManager(props);
-        protocols.add(mempoolManager);
-        BlockDirector<SlimTransaction, BlockContent<SlimTransaction>, LedgerBlock<BlockContent<SlimTransaction>, SortitionProof>, SortitionProof> blockDirector =
-                BlockDirectorKits.getSimpleLedgerBlock(props, mempoolManager, myKeys);
-        protocols.add((GenericProtocol) blockDirector);
-        TxsLoader<SlimTransaction> txsLoader = new TxLoaderImp(blockDirector);
-        Optional<TransactionGenerator> transactionGenerator =
-                loadTxsIfNecessary(props, myKeys, txsLoader);
-        transactionGenerator.ifPresent(protocols::add);
-        AccountManager accountManager = new StatefulAccountManager<>(props, mempoolManager);
-        protocols.add((GenericProtocol) accountManager);
-        KeyBlockManager keyBlockManager = new ConstantWeightLedgerKeyBlockManager<>(props);
-        protocols.add((GenericProtocol) keyBlockManager);
-        SybilProofValidator<SortitionProof> proofValidator = new SortitionProofValidator(props, accountManager);
-        BlockValidator<LedgerBlock<BlockContent<SlimTransaction>, SortitionProof>> appValidator =
-                new KeyAndMicroBlocksValidator(props, mempoolManager, proofValidator);
-        protocols.add((GenericProtocol) appValidator);
-        Ledger<LedgerBlock<BlockContent<SlimTransaction>, SortitionProof>> ledger =
-                new Blockchain<>(props, appValidator, new RichBootstrapModule());
-        BabelLedger<LedgerBlock<BlockContent<SlimTransaction>, SortitionProof>> babelLedger = new BabelLedger<>(ledger);
-        protocols.add(babelLedger);
-        GenericProtocol posSortitionElection =
-                new PoSAlgorandSortitionWithDRand<>(props, myKeys, accountManager,
-                keyBlockManager, ledger, blockDirector);
-        protocols.add(posSortitionElection);
-        protocols.addAll(addCommonDLProtocols(props, myKeys, ledger));
-        ledger.attachObserver(new UnfinalizedBlocksLog(props));
-        ledger.attachObserver(new FinalizedBlocksLog(props));
-        initializeProtocols(props, babel, protocols);
     }
 
     private static void launchBlockmess(Properties props, Host myself, Babel babel) throws Exception {
@@ -357,32 +223,12 @@ public class Main {
         return keys;
     }
 
-    private static Collection<SlimTransaction> getBootstrapTxs(Properties props, KeyPair myKeys,
-                                                               List<PublicKey> nodes, TxsLoader txsLoader)
-            throws IOException, NoSuchAlgorithmException, InvalidKeySpecException {
-        if (props.getProperty("loadTxs", "F").equals("T")) {
-            return loadBootstrapTxsFromFile(props, txsLoader);
-        } else {
-            return generateNewBootstrapTxs(props, myKeys, nodes);
-        }
-    }
-
     private static List<GenericProtocol> addNetworkProtocols(Properties props, Host myself) throws Exception {
         List<GenericProtocol> protocols = new LinkedList<>();
-        PeerSamplingProtocol peerSamplingProtocol = getPeerSamplingProtocol(props, myself);
+        PeerSamplingProtocol peerSamplingProtocol = new HyparView(props, myself);;
         protocols.add((GenericProtocol) peerSamplingProtocol);
         protocols.addAll(addBroadcastProtocols(props, myself, peerSamplingProtocol));
         protocols.add(new ValueDispatcher<>());
-        return protocols;
-    }
-
-    private static <P extends SybilElectionProof> List<GenericProtocol> addCommonDLProtocols(
-            Properties props, KeyPair myKeys, Ledger<LedgerBlock<BlockContent<SlimTransaction>, P>> ledger)
-            throws Exception {
-        List<GenericProtocol> protocols = new LinkedList<>();
-        protocols.add(new ValueDispatcher<>());
-        protocols.add(new SingleElementCommittee<>(ledger));
-        protocols.add(getDLClient(props, myKeys.getPublic()));
         return protocols;
     }
 
@@ -422,31 +268,6 @@ public class Main {
         txsLoader.loadTxs(structuredValues);
     }
 
-    private static Optional<TransactionGenerator> loadTxsIfNecessary(Properties props, KeyPair myKeys,
-                                                                     TxsLoader<SlimTransaction> txsLoader)
-            throws Exception {
-        if (props.getProperty("simplifiedTxs", "F").equals("T")) {
-            List<PublicKey> nodes = loadKeys(props);
-            Collection<SlimTransaction> txs = getBootstrapTxs(props, myKeys, nodes, txsLoader);
-            txsLoader.loadTxs(txs);
-            return Optional.empty();
-        } else {
-            return Optional.of(new TransactionGenerator(props, myKeys));
-        }
-    }
-
-    private static Collection<SlimTransaction> generateNewBootstrapTxs(Properties props, KeyPair myKeys, List<PublicKey> nodes) {
-        int numTxs = parseInt(props.getProperty("numBootstrapTxs", "10000"));
-        return new FakeTxsGenerator(myKeys)
-                .generateFakeTxs(nodes, numTxs);
-    }
-
-    private static Collection<SlimTransaction> loadBootstrapTxsFromFile(Properties props, TxsLoader txsLoader)
-            throws IOException, NoSuchAlgorithmException, InvalidKeySpecException {
-        String filepath = props.getProperty("loadTxsLocation");
-        return txsLoader.loadFromFile(filepath);
-    }
-
     private static void initializeProtocols(Properties props, Babel babel, List<GenericProtocol> protocols)
             throws ProtocolAlreadyExistsException, HandlerRegistrationException, IOException {
         for (GenericProtocol protocol : protocols)
@@ -454,33 +275,6 @@ public class Main {
         babel.start();
         for (GenericProtocol protocol : protocols) {
             protocol.init(props);
-        }
-    }
-
-    private static PeerSamplingProtocol getPeerSamplingProtocol(Properties props, Host myself)
-            throws Exception {
-        switch (props.getProperty("peerSamplingProtocol", "hyparview")) {
-            case "simpleFull":
-                return new SimpleFullMembership(props, myself);
-            case "hyparview":
-                return new HyparView(props, myself);
-            default:
-                logger.error("Cannot identify the Peer Sampling Protocol. Exiting");
-                System.exit(1);
-                throw new Exception();
-        }
-    }
-
-    private static GenericProtocol getDLClient(Properties props, PublicKey myKey) throws Exception {
-        switch (props.getProperty("client", "interactive")) {
-            case "interactive":
-                return new InteractiveClient(props, myKey);
-            case "automated":
-                return new AutomatedClient(props, myKey);
-            default:
-                System.err.println("Intended client protocol does not exist");
-                System.exit(1);
-                throw new Exception();
         }
     }
 
