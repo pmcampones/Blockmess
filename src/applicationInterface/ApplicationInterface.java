e24740cfc4abac3398600355ae72c9a2a513a8d0
elcampones <p.campones@campus.fct.unl.pt>
2022-10-06 13:26:27 +0100

    Introduce submission of local operations. - These operations are submitted to the local Blockmess instance, but are not disseminated throughout the network; - Other nodes will only receive these operations when they are placed in blocks, which are eventually disseminated.

--- src/applicationInterface/ApplicationInterface.java
+++ src/applicationInterface/ApplicationInterface.java
@@ -1,9 +1,10 @@
 package applicationInterface;
 
 import cmux.AppOperation;
-import cmux.FixedCMuxIdMapper;
 import ledger.LedgerObserver;
 import ledger.blocks.BlockmessBlock;
+import ledger.ledgerManager.LedgerManager;
+import lombok.SneakyThrows;
 import main.BlockmessLauncher;
 import mempoolManager.MempoolManager;
 import org.apache.commons.lang3.tuple.Pair;
@@ -11,7 +12,6 @@ import org.jetbrains.annotations.NotNull;
 import org.thavam.util.concurrent.blockingMap.BlockingHashMap;
 import org.thavam.util.concurrent.blockingMap.BlockingMap;
 import pt.unl.fct.di.novasys.babel.core.GenericProtocol;
-import pt.unl.fct.di.novasys.babel.exceptions.HandlerRegistrationException;
 import utils.IDGenerator;
 import validators.FixedApplicationAwareValidator;
 import valueDispatcher.ValueDispatcher;
@@ -32,177 +32,198 @@ import static org.bouncycastle.pqc.math.linearalgebra.ByteUtils.concatenate;
  * <p>This class maintains a record of the operations submitted by the client connected to this replica.
  * When one such operation is executed, the result of the operation is delivered to the application.</p>
  * <p>The underlying Blockmess structure is aware this class is connected to the application,
- * as such application specific logic is submitted to this abstract class.
- * In particular, the processing of the operations is submitted here.</p>
+ * as such application specific logic is submitted to this abstract class. In particular, the processing of the
+ * operations is submitted here.</p>
  * <p>In order to compute this application logic, the application must extend this class.</p>
  */
 public abstract class ApplicationInterface extends GenericProtocol implements LedgerObserver {
 
-    private final AtomicLong localOpIdx = new AtomicLong(0);
-    private long globalOpIdx = 0;
+	private final AtomicLong localOpIdx = new AtomicLong(0);
+	private final BlockingMap<UUID, Pair<byte[], Long>> completedSyncOperations = new BlockingHashMap<>();
+	private final Map<UUID, ReplyListener> operationListeners = new ConcurrentHashMap<>();
+	private final Set<UUID> operationsWaitingResponse = ConcurrentHashMap.newKeySet();
+	private final BlockingQueue<AppOperation> queuedOperations = new LinkedBlockingQueue<>();
+	private long globalOpIdx = 0;
+	private byte[] replicaId;
 
-    private final BlockingMap<UUID, Pair<byte[], Long>> completedSyncOperations = new BlockingHashMap<>();
+	/**
+	 * The creation of the ApplicationInterface triggers the launch of Blockmess.
+	 * <p>Upon the creation of this class, this replica will connect to others according with the launch
+	 * configurations, and is then ready to submit, receive, and execute operations and blocks</p>
+	 *
+	 * @param blockmessProperties A list of properties that override those in the default configuration file.
+	 *                            <p> This file is found in "${PWD}/config/config.properties"</p>
+	 */
+	@SneakyThrows
+	public ApplicationInterface(@NotNull String[] blockmessProperties) {
+		super(ApplicationInterface.class.getSimpleName(), IDGenerator.genId());
+		subscribeNotification(DeliverFinalizedContentNotification.ID, this::uponDeliverFinalizedContentNotification);
+		BlockmessLauncher.launchBlockmess(blockmessProperties, this);
+	}
 
-    private final Map<UUID, ReplyListener> operationListeners = new ConcurrentHashMap<>();
+	private void uponDeliverFinalizedContentNotification(DeliverFinalizedContentNotification notif, short source) {
+		queuedOperations.addAll(notif.getFinalizedContent());
+	}
 
-    private final Set<UUID> operationsWaitingResponse = ConcurrentHashMap.newKeySet();
+	@Override
+	public void init(Properties props) {
+		String address = props.getProperty("address");
+		int port = Integer.parseInt(props.getProperty("port"));
+		replicaId = concatenate(address.getBytes(), numToBytes(port));
+		new Thread(this::processOperations).start();
+		MempoolManager.getSingleton().attachObserver(this);
+	}
 
-    private final BlockingQueue<AppOperation> queuedOperations = new LinkedBlockingQueue<>();
+	private static byte[] numToBytes(long num) {
+		return ByteBuffer.allocate(Long.BYTES).putLong(num).array();
+	}
 
-    private byte[] replicaId;
+	private void processOperations() {
+		while (true) {
+			try {
+				AppOperation currentOp = queuedOperations.take();
+				byte[] operationResult = processOperation(currentOp.getContent());
+				if (operationsWaitingResponse.remove(currentOp.getId()))
+					replyOperationResults(currentOp, operationResult);
+				globalOpIdx++;
+			} catch (InterruptedException e) {
+				throw new RuntimeException(e);
+			}
+		}
+	}
 
-    /**
-     * The creation of the ApplicationInterface triggers the launch of Blockmess.
-     * <p>Upon the creation of this class, this replica will connect to others according with the launch configurations,
-     * and is then ready to submit, receive, and execute operations and blocks</p>
-     * @param blockmessProperties A list of properties that override those in the default configuration file.
-     *                            <p> This file is found in "${PWD}/config/config.properties"</p>
-     */
-    public ApplicationInterface(@NotNull String[] blockmessProperties) {
-        super(ApplicationInterface.class.getSimpleName(), IDGenerator.genId());
-        try {
-            subscribeNotification(DeliverFinalizedContentNotification.ID, this::uponDeliverFinalizedContentNotification);
-        } catch (HandlerRegistrationException e) {
-            throw new RuntimeException(e);
-        }
-        BlockmessLauncher.launchBlockmess(blockmessProperties, this);
-    }
+	private void replyOperationResults(AppOperation currentOp, byte[] operationResult) {
+		ReplyListener listener = operationListeners.remove(currentOp.getId());
+		Pair<byte[], Long> operationReply = Pair.of(operationResult, globalOpIdx);
+		if (listener != null) listener.processReply(operationReply);
+		else completedSyncOperations.put(currentOp.getId(), operationReply);
+	}
 
-    private void uponDeliverFinalizedContentNotification(DeliverFinalizedContentNotification notif, short source) {
-        queuedOperations.addAll(notif.getFinalizedContent());
-    }
+	/**
+	 * Locally processes an operation after it has been ordered throughout all the replicas.
+	 *
+	 * @param operation The generic operation to be processed.
+	 * @return The output of the operation processing.
+	 * <p>Should the replica the is executing this method be the one who submitted the operation,
+	 * the value returned here will be the result received in the invoke operations</p>
+	 */
+	public abstract byte[] processOperation(byte[] operation);
 
-    @Override
-    public void init(Properties props) {
-        String address = props.getProperty("address");
-        int port = Integer.parseInt(props.getProperty("port"));
-        replicaId = concatenate(address.getBytes(), numToBytes(port));
-        new Thread(this::processOperations).start();
-        MempoolManager.getSingleton().attachObserver(this);
-    }
+	/**
+	 * Submits an operation and blocks the calling thread until the operation is processed.
+	 *
+	 * @param operation The generic operation to be disseminated, ordered, and processed.
+	 * @return A pair containing the result of the parameter operation and the global operation index of the operation's
+	 * execution.
+	 * <p>A value of X in the right hand side means the operation was the Xth to be processed globally.</p>
+	 */
+	@SneakyThrows
+	public Pair<byte[], Long> invokeSyncOperation(byte @NotNull [] operation) {
+		var isValid = FixedApplicationAwareValidator.getSingleton().validateReceivedOperation(operation);
+		return isValid.getLeft() ? invokeValidSyncOperation(operation) : Pair.of(isValid.getRight(), -1L);
+	}
 
-    private static byte[] numToBytes(long num) {
-        return ByteBuffer.allocate(Long.BYTES).putLong(num).array();
-    }
+	private Pair<byte[], Long> invokeValidSyncOperation(byte[] operation) throws InterruptedException {
+		AppOperation content = computeAppContent(operation);
+		operationsWaitingResponse.add(content.getId());
+		ValueDispatcher.getSingleton().disseminateAppContentRequest(content);
+		return completedSyncOperations.take(content.getId());
+	}
 
-    private void processOperations() {
-        while(true) {
-            try {
-                AppOperation currentOp = queuedOperations.take();
-                byte[] operationResult = processOperation(currentOp.getContent());
-                if (operationsWaitingResponse.remove(currentOp.getId()))
-                    replyOperationResults(currentOp, operationResult);
-                globalOpIdx++;
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-        }
-    }
+	private AppOperation computeAppContent(byte[] operation) {
+		byte[] replicaMetadata = makeMetadata();
+		return new AppOperation(operation, replicaMetadata);
+	}
 
-    private void replyOperationResults(AppOperation currentOp, byte[] operationResult) {
-        ReplyListener listener = operationListeners.remove(currentOp.getId());
-        Pair<byte[], Long> operationReply = Pair.of(operationResult, globalOpIdx);
-        if (listener != null) listener.processReply(operationReply);
-        else completedSyncOperations.put(currentOp.getId(), operationReply);
-    }
+	private byte[] makeMetadata() {
+		byte[] opIdx = numToBytes(localOpIdx.getAndIncrement());
+		return concatenate(replicaId, opIdx);
+	}
 
-    /**
-     * Submits an operation and blocks the calling thread until the operation is processed.
-     * @param operation The generic operation to be disseminated, ordered, and processed.
-     * @return A pair containing the result of the parameter operation
-     * and the global operation index of the operation's execution.
-     * <p>A value of X in the right hand side means the operation was the Xth to be processed globally.</p>
-     */
-    public Pair<byte[], Long> invokeSyncOperation(byte @NotNull [] operation) {
-        try {
-            return tryToInvokeSyncOperation(operation);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-    }
+	@SneakyThrows
+	public Pair<byte[], Long> invokeLocalSyncOperation(byte @NotNull [] operation) {
+		var isValid = FixedApplicationAwareValidator.getSingleton().validateReceivedOperation(operation);
+		return isValid.getLeft() ? invokeValidSyncOperation(operation) : Pair.of(isValid.getRight(), -1L);
+	}
 
-    private Pair<byte[], Long> tryToInvokeSyncOperation(byte[] operation) throws InterruptedException {
-        var isValid = FixedApplicationAwareValidator.getSingleton().validateReceivedOperation(operation);
-        return isValid.getLeft() ? invokeValidSyncOperation(operation) : Pair.of(isValid.getRight(), -1L);
-    }
+	private Pair<byte[], Long> invokeValidLocalSyncOperation(byte[] operation) throws InterruptedException {
+		AppOperation content = computeAppContent(operation);
+		operationsWaitingResponse.add(content.getId());
+		LedgerManager.getSingleton().submitOperation(content);
+		return completedSyncOperations.take(content.getId());
+	}
 
-    private Pair<byte[], Long> invokeValidSyncOperation(byte[] operation) throws InterruptedException {
-        AppOperation content = computeAppContent(operation);
-        operationsWaitingResponse.add(content.getId());
-        ValueDispatcher.getSingleton().disseminateAppContentRequest(content);
-        return completedSyncOperations.take(content.getId());
-    }
+	/**
+	 * Submits an operation but does not block the calling thread.
+	 *
+	 * @param operation The generic operation to be disseminated, ordered, and processed.
+	 * @param listener  The structure that will receive and process the result of the operation.
+	 */
+	public void invokeAsyncOperation(byte @NotNull [] operation, @NotNull ReplyListener listener) {
+		var isValid = FixedApplicationAwareValidator.getSingleton().validateReceivedOperation(operation);
+		if (isValid.getLeft()) invokeValidAsyncOperation(operation, listener);
+		else listener.processReply(Pair.of(isValid.getRight(), -1L));
+	}
 
-    /**
-     * Submits an operation but does not block the calling thread.
-     * @param operation The generic operation to be disseminated, ordered, and processed.
-     * @param listener The structure that will receive and process the result of the operation.
-     */
-    public void invokeAsyncOperation(byte @NotNull [] operation, @NotNull ReplyListener listener) {
-        var isValid = FixedApplicationAwareValidator.getSingleton().validateReceivedOperation(operation);
-        if (isValid.getLeft()) invokeValidAsyncOperation(operation, listener);
-        else listener.processReply(Pair.of(isValid.getRight(), -1L));
-    }
+	private void invokeValidAsyncOperation(byte[] operation, ReplyListener listener) {
+		AppOperation content = computeAppContent(operation);
+		operationsWaitingResponse.add(content.getId());
+		operationListeners.put(content.getId(), listener);
+		ValueDispatcher.getSingleton().disseminateAppContentRequest(content);
+	}
 
-    private AppOperation computeAppContent(byte[] operation) {
-        FixedCMuxIdMapper mapper = FixedCMuxIdMapper.getSingleton();
-        byte[] replicaMetadata = makeMetadata();
-        return new AppOperation(operation, replicaMetadata);
-    }
+	public void invokeLocalAsyncOperation(byte @NotNull [] operation, @NotNull ReplyListener listener) {
+		var isValid = FixedApplicationAwareValidator.getSingleton().validateReceivedOperation(operation);
+		if (isValid.getLeft()) invokeValidAsyncOperation(operation, listener);
+		else listener.processReply(Pair.of(isValid.getRight(), -1L));
+	}
 
-    private void invokeValidAsyncOperation(byte @NotNull [] operation, @NotNull ReplyListener listener) {
-        AppOperation content = computeAppContent(operation);
-        operationsWaitingResponse.add(content.getId());
-        operationListeners.put(content.getId(), listener);
-        ValueDispatcher.getSingleton().disseminateAppContentRequest(content);
-    }
+	private void invokeValidLocalAsyncOperation(byte[] operation, ReplyListener listener) {
+		AppOperation content = computeAppContent(operation);
+		operationsWaitingResponse.add(content.getId());
+		operationListeners.put(content.getId(), listener);
+		LedgerManager.getSingleton().submitOperation(content);
+	}
 
-    private byte[] makeMetadata() {
-        byte[] opIdx = numToBytes(localOpIdx.getAndIncrement());
-        return concatenate(replicaId, opIdx);
-    }
+	@Override
+	public void deliverNonFinalizedBlock(BlockmessBlock block, int weight) {
+		notifyNonFinalizedBlock(block);
+	}
 
-    /**
-     * Locally processes an operation after it has been ordered throughout all the replicas.
-     * @param operation The generic operation to be processed.
-     * @return The output of the operation processing.
-     * <p>Should the replica the is executing this method be the one who submitted the operation,
-     * the value returned here will be the result received in the invoke operations</p>
-     */
-    public abstract byte[] processOperation(byte[] operation);
+	/**
+	 * Delivers a block to the application when it is received by this replica.
+	 * <p>The default behavior for this operation is doing nothing,
+	 * given that the application should be independent from the Blockmess logic.</p>
+	 * <p>Nevertheless, this operation may be useful to extract some metrics.</p>
+	 * <p>In order to make use of this operation, the class extending this one must override this method.</p>
+	 *
+	 * @param block The block just received. It has not yet been finalized.
+	 *              <p>The content in the block is still encapsulated with the metadata required for Blockmess to
+	 *              process the block</p>
+	 */
+	public void notifyNonFinalizedBlock(BlockmessBlock block) {
+	}
 
-    @Override
-    public void deliverNonFinalizedBlock(BlockmessBlock block, int weight) {
-        notifyNonFinalizedBlock(block);
-    }
+	@Override
+	public void deliverFinalizedBlocks(List<UUID> finalized, Set<UUID> discarded) {
+		notifyFinalizedBlocks(finalized, discarded);
+	}
 
-    /**
-     * Delivers a block to the application when it is received by this replica.
-     * <p>The default behavior for this operation is doing nothing,
-     * given that the application should be independent from the Blockmess logic.</p>
-     * <p>Nevertheless, this operation may be useful to extract some metrics.</p>
-     * <p>In order to make use of this operation, the class extending this one must override this method.</p>
-     * @param block The block just received. It has not yet been finalized.
-     *              <p>The content in the block is still encapsulated with the metadata required for Blockmess to process the block</p>
-     */
-    public void notifyNonFinalizedBlock(BlockmessBlock block){}
-
-    @Override
-    public void deliverFinalizedBlocks(List<UUID> finalized, Set<UUID> discarded) {
-        notifyFinalizedBlocks(finalized, discarded);
-    }
-
-    /**
-     * Delivers the identifiers of blocks that have been finalized.
-     * <p>The default behavior for this operation is doing nothing,
-     * given that the application should be independent from the Blockmess logic.</p>
-     * <p>Nevertheless, this operation may be useful to extract some metrics.</p>
-     * <p>In order to make use of this operation, the class extending this one must override this method.</p>
-     * @param finalized The identifiers of blocks that have been finalized.
-     *                  <p>The blocks referred by this identifiers can be reached by storing them during the @notifyNonFinalizedBlock method execution</p>
-     * @param discarded The identifiers of blocks discarded by Blockmess.
-     *                  <p>A block may be discarded if it at some point forked the longest chain in a {@link ledger.blockchain.Blockchain} instance.</p>
-     */
-    public void notifyFinalizedBlocks(List<UUID> finalized, Set<UUID> discarded){}
+	/**
+	 * Delivers the identifiers of blocks that have been finalized.
+	 * <p>The default behavior for this operation is doing nothing,
+	 * given that the application should be independent from the Blockmess logic.</p>
+	 * <p>Nevertheless, this operation may be useful to extract some metrics.</p>
+	 * <p>In order to make use of this operation, the class extending this one must override this method.</p>
+	 *
+	 * @param finalized The identifiers of blocks that have been finalized.
+	 *                  <p>The blocks referred by this identifiers can be reached by storing them during the
+	 * @param discarded The identifiers of blocks discarded by Blockmess.
+	 *                  <p>A block may be discarded if it at some point forked the longest chain in a
+	 *                  {@link ledger.blockchain.Blockchain} instance.</p>
+	 * @notifyNonFinalizedBlock method execution</p>
+	 */
+	public void notifyFinalizedBlocks(List<UUID> finalized, Set<UUID> discarded) {
+	}
 
 }
--- src/valueDispatcher/ValueDispatcher.java
+++ src/valueDispatcher/ValueDispatcher.java
@@ -19,95 +19,98 @@ import java.util.Properties;
 
 /**
  * Middleware that separates the DL engine and Application protocols from the Broadcast and Peer Sampling protocols.
- * <p>Receives requests from the DL engine and Application to disseminate values and decides the Broadcast protocol to be used.</p>
+ * <p>Receives requests from the DL engine and Application to disseminate values and decides the Broadcast protocol to
+ * be used.</p>
  * <p>All content pertaining to the DL engine and Application from the Broadcast Protocols is delivered here.</p>
  * <p>The values disseminated are processed in 3 stages, if another is added, the following should be done.</p>
- *  <p>1st - Introduce a new type of message in the ValType enum. This enable the Dispatcher to identify values delivered from the Broadcasts.</p>
- *  <p>2nd - Register a request handler for the message type. It must contain a BroadcastValue instance,
- *      as these instances contain the necessary logic to be serialized by the Broadcast protocols irrespective of their contents.</p>
- *  <p>3rd - Add a new entry to the switch to forward the inner BroadcastValue to the pertinent protocols.</p>
- *      Doing a verification of the BroadcastValue type here is important but not necessary.
- *      Verifying here ensures malformed content is discarded without upsetting the logic of the more complex upper protocols.
+ * <p>1st - Introduce a new type of message in the ValType enum. This enable the Dispatcher to identify values
+ * delivered
+ * from the Broadcasts.</p>
+ * <p>2nd - Register a request handler for the message type. It must contain a BroadcastValue instance,
+ * as these instances contain the necessary logic to be serialized by the Broadcast protocols irrespective of their
+ * contents.</p>
+ * <p>3rd - Add a new entry to the switch to forward the inner BroadcastValue to the pertinent protocols.</p>
+ * Doing a verification of the BroadcastValue type here is important but not necessary. Verifying here ensures malformed
+ * content is discarded without upsetting the logic of the more complex upper protocols.
  */
 public class ValueDispatcher extends GenericProtocol {
 
-    private static final Logger logger = LogManager.getLogger(ValueDispatcher.class);
+	public static final short ID = IDGenerator.genId();
+	private static final Logger logger = LogManager.getLogger(ValueDispatcher.class);
+	//If the val is not of the correct instance, its assumed the message is incorrect and thus ignored.
+	public static ValueDispatcher singleton;
 
-    public static final short ID = IDGenerator.genId();
+	private ValueDispatcher() {
+		super(ValueDispatcher.class.getSimpleName(), ID);
+		try {
+			tryToSetupDispatcher();
+		} catch (HandlerRegistrationException e) {
+			throw new RuntimeException(e);
+		}
+	}
 
-    //If the val is not of the correct instance, its assumed the message is incorrect and thus ignored.
-    public static ValueDispatcher singleton;
+	private void tryToSetupDispatcher() throws HandlerRegistrationException {
+		subscribeNotification(DeliverVal.ID, this::uponDeliverVal);
+	}
 
-    private ValueDispatcher() {
-        super(ValueDispatcher.class.getSimpleName(), ID);
-        try {
-            tryToSetupDispatcher();
-        } catch (HandlerRegistrationException e) {
-            throw new RuntimeException(e);
-        }
-    }
+	private void uponDeliverVal(DeliverVal v, short id) {
+		DispatcherWrapper wrapper = v.getVal();
+		short typeIndex = wrapper.getDispatcherType();
+		if (typeIndex < ValType.values().length) {
+			ValType type = ValType.values()[typeIndex];
+			logger.info("Received disseminated content from {} of type {}", id, type);
+			notifyUpperProtocols(type, wrapper.getVal());
+		}
+	}
 
-    private void tryToSetupDispatcher() throws HandlerRegistrationException {
-        subscribeNotification(DeliverVal.ID, this::uponDeliverVal);
-    }
+	private void notifyUpperProtocols(ValType type, BroadcastValue val) {
+		switch (type) {
+			case APP_CONTENT:
+				if (val instanceof AppOperation)
+					LedgerManager.getSingleton().submitOperation((AppOperation) val);
+				break;
+			case SIGNED_BLOCK:
+				if (val instanceof BlockmessBlock)
+					LedgerManager.getSingleton().submitBlock((BlockmessBlock) val);
+				break;
+			default:
+				logger.debug("Received unknown value type");
+		}
+	}
 
-    public static ValueDispatcher getSingleton() {
-        if (singleton == null)
-            singleton = new ValueDispatcher();
-        return singleton;
-    }
+	public static ValueDispatcher getSingleton() {
+		if (singleton == null)
+			singleton = new ValueDispatcher();
+		return singleton;
+	}
 
-    @Override
-    public void init(Properties properties) {}
+	@Override
+	public void init(Properties properties) {
+	}
 
-    private void uponDeliverVal(DeliverVal v, short id) {
-        DispatcherWrapper wrapper = v.getVal();
-        short typeIndex = wrapper.getDispatcherType();
-        if (typeIndex < ValType.values().length) {
-            ValType type = ValType.values()[typeIndex];
-            logger.info("Received disseminated content from {} of type {}", id, type);
-            notifyUpperProtocols(type, wrapper.getVal());
-        }
-    }
+	public void disseminateBlockRequest(BlockmessBlock block) {
+		logger.info("Requested the dissemination of a {}", ValType.SIGNED_BLOCK);
+		sendLazyRequest(block, ValType.SIGNED_BLOCK);
+	}
 
-    private void notifyUpperProtocols(ValType type, BroadcastValue val) {
-        switch (type) {
-            case APP_CONTENT:
-                if (val instanceof AppOperation)
-                    LedgerManager.getSingleton().submitOperation((AppOperation) val);
-                break;
-            case SIGNED_BLOCK:
-                if (val instanceof BlockmessBlock)
-                    LedgerManager.getSingleton().submitBlock((BlockmessBlock) val);
-                break;
-            default:
-                logger.debug("Received unknown value type");
-        }
-    }
+	private void sendLazyRequest(BroadcastValue val, ValType type) {
+		DispatcherWrapper wrapper = new DispatcherWrapper((short) type.ordinal(), val);
+		sendRequest(new LazyBroadcastRequest(wrapper), LazyPushBroadcast.ID);
+	}
 
-    public void disseminateBlockRequest(BlockmessBlock block) {
-        logger.info("Requested the dissemination of a {}", ValType.SIGNED_BLOCK);
-        sendLazyRequest(block, ValType.SIGNED_BLOCK);
-    }
+	public void disseminateAppContentRequest(AppOperation req) {
+		logger.info("Requested the dissemination of a {}", ValType.APP_CONTENT);
+		sendEagerRequest(req, ValType.APP_CONTENT);
+	}
 
-    private void sendLazyRequest(BroadcastValue val, ValType type) {
-        DispatcherWrapper wrapper = new DispatcherWrapper((short) type.ordinal(), val);
-        sendRequest(new LazyBroadcastRequest(wrapper), LazyPushBroadcast.ID);
-    }
+	private void sendEagerRequest(BroadcastValue val, ValType type) {
+		DispatcherWrapper wrapper = new DispatcherWrapper((short) type.ordinal(), val);
+		sendRequest(new EagerBroadcastRequest(wrapper), EagerPushBroadcast.ID);
+	}
 
-    public void disseminateAppContentRequest(AppOperation req) {
-        logger.info("Requested the dissemination of a {}", ValType.APP_CONTENT);
-        sendEagerRequest(req, ValType.APP_CONTENT);
-    }
-
-    private void sendEagerRequest(BroadcastValue val, ValType type) {
-        DispatcherWrapper wrapper = new DispatcherWrapper((short) type.ordinal(), val);
-        sendRequest(new EagerBroadcastRequest(wrapper), EagerPushBroadcast.ID);
-    }
-
-    private enum ValType {
-        APP_CONTENT,                //A transaction is being broadcast
-        SIGNED_BLOCK,               //A Ledger Block is being broadcast
-    }
+	private enum ValType {
+		APP_CONTENT,
+		SIGNED_BLOCK,
+	}
 
 }
