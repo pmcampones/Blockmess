e39fec31968404f4446b75baadc1a63afef16d36
elcampones <p.campones@campus.fct.unl.pt>
2022-10-17 21:05:57 +0100

    Better Logging and Cryptocurrency running script

---
+++ scripts/automated_cryptocurrency/container/async.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+NUM_REPLICAS=$1
+INTERVAL=$2
+KEYS=$3
+
+source scripts/docker_init.sh
+
+
+if [ -z $NUM_REPLICAS ] || [ -z $INTERVAL ] || [ -z $KEYS ];
+then
+  echo "$1 <Number of replicas> <Interval from Tx proposals> <Public Keys Pathname>"
+  echo "Number of replicas: Dictates how may replicas will be used in this execution."
+  echo "Interval from Tx proposals: Interval of time between transaction proposals for a single node."
+  echo "Public Keys Pathname: The pathname of the file containing the pathnames of the public keys of all nodes."
+  exit
+fi
+
+CONTACT_PORT=6000
+for REPLICA_IDX in $(seq 0 $(( NUM_REPLICAS - 1)) )
+do
+  eval "docker container run -d --name replica$REPLICA_IDX --net blockmess_network --cap-add=NET_ADMIN blockmess_csd demo.cryptocurrency.client.AutomatedClient $INTERVAL $KEYS address=replica$REPLICA_IDX contact=replica0:$CONTACT_PORT"
+  echo "Replica $REPLICA_IDX running $FILE_LOC proposing with an interval of $INTERVAL"
+  sleep 2
+done
