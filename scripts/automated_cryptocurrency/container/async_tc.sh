3bdc4c7c4ae3307f90fc67e63a8e11a259dc2c3c
elcampones <p.campones@campus.fct.unl.pt>
2022-10-20 13:45:09 +0100

    Test Blockmess with cryptocurrency application while simulating latency between nodes.

---
+++ scripts/automated_cryptocurrency/container/async_tc.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+NUM_REPLICAS=$1
+INTERVAL=$2
+KEYS=$3
+
+
+eval "docker image build -t blockmess_tc -f Dockerfile_Traffic_Control ."
+
+if [ -z $NUM_REPLICAS ] || [ -z $INTERVAL ] || [ -z $KEYS ];
+then
+  echo "$1 <Number of replicas> <Interval from Tx proposals> <Public Keys Pathname>"
+  echo "Number of replicas: Dictates how may replicas will be used in this execution."
+  echo "Interval from Tx proposals: Interval of time between transaction proposals for a single node."
+  echo "Public Keys Pathname: The pathname of the file containing the pathnames of the public keys of all nodes."
+  exit
+fi
+
+CONTACT_PORT=6000
+for REPLICA_IDX in $(seq 0 $(( NUM_REPLICAS - 1)) )
+do
+  eval 'docker container run -d --name replica$REPLICA_IDX --net blockmess_network --cap-add=NET_ADMIN blockmess_tc bash -c "tc qdisc add dev eth0 root netem delay 75ms && java -cp target/BlockmessLib.jar demo.cryptocurrency.client.AutomatedClient $INTERVAL $KEYS address=replica$REPLICA_IDX contact=replica0:$CONTACT_PORT myPublic=./keys/public_$(( REPLICA_IDX + 1 )).pem mySecret=./keys/secret_$(( REPLICA_IDX + 1 )).pem interface=eth0"'
+  echo "Replica $REPLICA_IDX running $FILE_LOC proposing with an interval of $INTERVAL"
+  sleep 2
+done
